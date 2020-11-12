4be4819a9006c4ebf1a27d4f9daeb5bbfb70efd0
SEYEON_PARK <fourleaf123@naver.com>
2020-11-12 23:54:18 +0900

    Python언어 list(리스트) 연습!(2)

---
+++ PythonApplication11.py
@@ -0,0 +1,26 @@
+dept_list=["정통", "소프", "소프"]
+
+final_list=dept_list
+
+print(dept_list)
+print(final_list)
+
+final_list[0]="컴퓨터"
+
+print(dept_list)
+print(final_list)
+
+a=[1,2,3,4,5]
+b=[5,4,6]
+
+print(a+b)
+print(a*2)
+print('1' in a)
+print('1' not in a)
+
+a.sort()
+print(a)
+
+c=sorted(b)
+print(b)
+print(c)
